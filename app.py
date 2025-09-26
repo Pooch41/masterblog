c5d204d49f5ae9f436f6cde81090d119d9a61865
Pooch41 <d.kulikovski@gmail.com>
2025-09-26 15:44:59 +0200

    Data structures, and basic INDEX functionality.

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    import json
+
+    with open('data/blog.json', 'r') as file:
+        blog_posts = json.load(file)
+
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
